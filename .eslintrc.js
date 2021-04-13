7193173ce960a8701f40596c489b2efe0f71facc
thaicodingdev <67307892+thaicodingdev@users.noreply.github.com>
2021-04-13 20:11:43 +0700

    chore: use ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['node_modules'],
+  extends: '@thaicoding/eslint-config',
+}
