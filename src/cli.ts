80f21204195e9345de7046e3a0554c5b1626ebee
thaicodingdev <67307892+thaicodingdev@users.noreply.github.com>
2021-04-13 23:09:10 +0700

    refactor: show info when files do not exist

--- src/cli.ts
+++ src/cli.ts
@@ -1,33 +1,42 @@
 #!/usr/bin/env node
 
 import execa from 'execa'
-import fs from 'fs'
+import {existsSync} from 'fs'
 import ora from 'ora'
 import path from 'path'
 
 const cwd = process.cwd()
-const packageJsonPath = path.join(cwd, 'package.json')
-const isNpmPackage = fs.existsSync(packageJsonPath)
+const isNpmPackage = existsSync(path.join(cwd, 'package.json'))
 if (isNpmPackage) main()
 else console.log('Not an npm package')
 
 async function main() {
+  const hasNodeModules = existsSync(path.join(cwd, 'node_modules'))
   const spinner = ora('Removing node_modules')
   spinner.start()
-  try {
-    await execa('rm', ['-rf', 'node_modules'])
-    spinner.succeed()
-  } catch (error) {
-    spinner.stop()
-    console.error(error.message)
+  if (hasNodeModules) {
+    try {
+      await execa('rm', ['-rf', 'node_modules'])
+      spinner.succeed()
+    } catch (error) {
+      spinner.stop()
+      console.error(error.message)
+    }
+  } else {
+    spinner.info('no node_modules')
   }
 
+  const hasPagkageLockJson = existsSync(path.join(cwd, 'package-lock.json'))
   spinner.text = 'Removing package-lock.json'
   spinner.start()
-  try {
-    await execa('rm', ['package-lock.json'])
-    spinner.succeed()
-  } catch (error) {
+  if (hasPagkageLockJson) {
+    try {
+      await execa('rm', ['package-lock.json'])
+      spinner.succeed()
+    } catch (error) {
+      spinner.info('no package-lock.json')
+    }
+  } else {
     spinner.info('no package-lock.json')
   }
 
